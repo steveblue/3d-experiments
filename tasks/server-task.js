78fb3f4e9881778174d67514e82b6d6be580d5b2
Steve Belovarich <steve@installationspace.com>
2016-01-12 22:25:06 -0800

    Fixes for https server

--- tasks/server-task.js
+++ tasks/server-task.js
@@ -124,10 +124,10 @@ gulp.task('server:prod:start', function(callback){
 
   // Redirect non-www
 
-  server.get('/*', function(req, res, next) {
-    //console.log(req.hostname);
-    if (req.headers.host.match(/^www/) !== null ) {
-      res.redirect(301, 'http://' + req.headers.host.replace(/^www\./, '') + req.url);
+  server.get('*', function(req, res, next) {
+    console.log(req);
+    if ( req.headers.host !== options.prod.host ) {
+      res.redirect(301, 'https://' + options.prod.host + req.url);
     } else {
       next();
     }
